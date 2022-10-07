3dc4fbf0dbbd7f60326f05189123461f05e6e80b
SKefir <decskt@yandex.ru>
2022-10-07 17:14:47 +0200

    add junit callbacks

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,13 +1,16 @@
 package tests;
 
 
+import com.codeborne.selenide.Selenide;
 import config.TestConfiguration;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.apache.commons.configuration2.Configuration;
+import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class BaseTest implements BeforeAllCallback {
+public class BaseTest implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback {
     private static final Configuration CONFIG = TestConfiguration.getConfiguration();
 
     @Override
@@ -15,5 +18,15 @@ public class BaseTest implements BeforeAllCallback {
         if (CONFIG.getString("eleinide.driverPath", null) == null) {
             ChromeDriverManager.getInstance().setup();
         }
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        Selenide.closeWebDriver();
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        Selenide.open(CONFIG.getString("urls.metaUrl"));
     }
 }
