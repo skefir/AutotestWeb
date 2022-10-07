d3e34ef8f779ad2337b11a35ce4160c45b8807af
SKefir <decskt@yandex.ru>
2022-10-07 17:13:36 +0200

    update configuration

--- src/test/java/config/TestConfiguration.java
+++ src/test/java/config/TestConfiguration.java
@@ -1,17 +1,36 @@
 package config;
 
+import com.codeborne.selenide.Configuration;
 import lombok.SneakyThrows;
 import lombok.Synchronized;
-import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.lang3.StringUtils;
 
 public class TestConfiguration {
-    private static Configuration instance;
+    private static org.apache.commons.configuration2.Configuration instance;
 
     private TestConfiguration() {
+    }
+
+    private static void createConfig() {
+        loadConfiguration();
+        Configuration.timeout = instance.getInt("seleinide.timout", 10000);
+        String browserName = instance.getString("seleinide.browser.name", null);
+        if (StringUtils.isNotBlank(browserName)) {
+            Configuration.browser = browserName;
+        }
+        String browserVersion = instance.getString("seleinide.browser.version", null);
+        if (StringUtils.isNotBlank(browserVersion)) {
+            Configuration.browserVersion = browserVersion;
+        }
+        String remoteUrl = instance.getString("selenide.remote.url", null);
+        if (StringUtils.isNotBlank(remoteUrl)) {
+            Configuration.remote = remoteUrl;
+        }
+        Configuration.browserSize = instance.getString("selenide.browser.size", "1920x1080");
     }
 
     @SneakyThrows
@@ -26,9 +45,9 @@ public class TestConfiguration {
         }
     }
 
-    public static Configuration getConfiguration() {
+    public static org.apache.commons.configuration2.Configuration getConfiguration() {
         if (instance == null) {
-            loadConfiguration();
+            createConfig();
         }
         return instance;
     }
