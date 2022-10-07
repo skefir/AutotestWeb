9cb1cfb6d5b12b6906836eac0533bfdc5240b19a
SKefir <decskt@yandex.ru>
2022-10-07 17:40:00 +0200

    add ui test

--- src/test/java/config/BaseTestExtension.java
+++ src/test/java/config/BaseTestExtension.java
@@ -1,16 +1,23 @@
-package tests;
+package config;
 
 
 import com.codeborne.selenide.Selenide;
 import config.TestConfiguration;
+import data.Currencies;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.apache.commons.configuration2.Configuration;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import page.CalendarPage;
 
-public class BaseTest implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback {
+import java.util.EnumSet;
+import java.util.Set;
+
+
+public class BaseTestExtension implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback {
     private static final Configuration CONFIG = TestConfiguration.getConfiguration();
 
     @Override
@@ -29,4 +36,13 @@ public class BaseTest implements BeforeAllCallback, BeforeEachCallback, AfterEac
     public void beforeEach(ExtensionContext context) throws Exception {
         Selenide.open(CONFIG.getString("urls.metaUrl"));
     }
+
+    private final CalendarPage calendarPage = new CalendarPage();
+
+    @Test
+    public void expTest() {
+        Set<Currencies> currenciesSet = EnumSet.of(Currencies.CHF);
+        calendarPage.setCurrenciesFilter(currenciesSet);
+    }
 }
+
--- src/test/java/tests/CalendarTests.java
+++ src/test/java/tests/CalendarTests.java
@@ -1,13 +1,24 @@
 package tests;
 
+import config.BaseTestExtension;
+import data.Currencies;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import page.CalendarPage;
+
+import java.util.EnumSet;
+import java.util.Set;
 
 @Slf4j
-public class CalendarTests extends BaseTest {
+@ExtendWith({BaseTestExtension.class})
+public class CalendarTests {
+
+    private final CalendarPage calendarPage = new CalendarPage();
 
     @Test
     public void expTest() {
-        log.error("exp!!!!");
+        Set<Currencies> currenciesSet = EnumSet.of(Currencies.CHF);
+        calendarPage.setCurrenciesFilter(currenciesSet);
     }
 }
