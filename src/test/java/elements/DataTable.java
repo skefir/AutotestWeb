1f10b35891cd8a0834648904b0ca5031dc7c38bc
SKefir <decskt@yandex.ru>
2022-10-09 13:10:26 +0200

    refactor DataTable + fix enterToEvent

--- src/test/java/elements/DataTable.java
+++ src/test/java/elements/DataTable.java
@@ -1,11 +1,15 @@
 package elements;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import data.DataTableColumn;
+import lombok.Synchronized;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DataTable<E extends DataTableColumn> {
 
@@ -13,22 +17,44 @@ public class DataTable<E extends DataTableColumn> {
 
     private final Set<E> columns;
 
-    private final Map<E, Integer> columnsNumbers;
+    private Map<E, Integer> columnsNumbers = null;
 
     public DataTable(SelenideElement rootElement, Set<E> columns) {
         this.rootElement = rootElement;
         this.columns = columns;
-        columnsNumbers = new HashMap<>();
     }
 
     private int getCollumnNumber(E column) {
         if (!columns.contains(column)) {
             throw new IllegalArgumentException("Illegal column - " + column);
         }
+        if (columnsNumbers == null) {
+            calculateColumnNumbers();
+        }
+
         return columnsNumbers.computeIfAbsent(column,
-                k -> rootElement.$$x(".//div[@class='ec-table__header']//div[contains(@class,'ec-table__col') " +
-                        "and contains(text()[1],'" + column.getTitle()
-                        + "')]//preceding-sibling::div[contains(@class,'ec-table__col')]").size() + 1);
+                k -> {
+                    throw new IllegalStateException("Can't find key " + column + " in " + getTableHeaders());
+                });
+    }
+
+    @Synchronized
+    private void calculateColumnNumbers() {
+        if (columnsNumbers == null) {
+            columnsNumbers = new HashMap<>();
+            List<String> headerList = getTableHeaders().asFixedIterable()
+                    .stream().map(element -> element.getOwnText().trim()).collect(Collectors.toList());
+            columns.forEach(column -> {
+                if (headerList.indexOf(column.getTitle()) != -1) {
+                    columnsNumbers.put(column, headerList.indexOf(column.getTitle()));
+                }
+            });
+        }
+    }
+
+    private ElementsCollection getTableHeaders() {
+        return rootElement
+                .$$(".ec-table__header .ec-table__col");
     }
 
     public SelenideElement getRowByNumber(int rowNumber) {
@@ -36,6 +62,6 @@ public class DataTable<E extends DataTableColumn> {
     }
 
     public SelenideElement getColumn(SelenideElement rowElement, E column) {
-        return rowElement.$x(".//div[contains(@class,'ec-table__col')][" + getCollumnNumber(column) + "]");
+        return rowElement.$$(".ec-table__col").get(getCollumnNumber(column));
     }
 }
